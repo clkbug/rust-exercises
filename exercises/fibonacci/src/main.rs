7f9a51a9be47a35ae2e31cc5f33a42f5fcff2a2f
Akifumi Fukuda <a.fukuda@mtl.t.u-tokyo.ac.jp>
2017-12-04 11:37:11 +0900

    solve 09.fibonacci

--- exercises/fibonacci/src/main.rs
+++ exercises/fibonacci/src/main.rs
@@ -4,19 +4,24 @@
 
 fn main() {
     let n = 40;
-    let memo = vec![-1; n + 1];
+    let mut memo = vec![-1; n + 1];
 
     for i in 0..n + 1 {
-        let result = fib(i, &memo);
+        let result = fib(i, &mut memo);
         println!("fib({}) = {}", i, result);
     }
 }
 
-fn fib(n: usize, memo: &Vec<i32>) -> i32 {
-    match n {
-        0 => 0,
-        1 | 2 => 1,
-        _ => fib(n - 1, memo) + fib(n - 2, memo),
+fn fib(n: usize, memo: &mut Vec<i32>) -> i32 {
+    if memo[n] != -1 {
+        memo[n]
+    } else {
+        let t = match n {
+            0 => 0,
+            1 | 2 => 1,
+            _ => fib(n - 1, memo) + fib(n - 2, memo),
+        };
+        memo[n] = t;
+        t
     }
 }
-
